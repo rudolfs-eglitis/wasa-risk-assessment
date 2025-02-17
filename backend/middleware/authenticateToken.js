b7184c5e66a022c5dd5b69f3e1fca24349d0be39
rudolfs-eglitis <rudolfs.eglitis@gmail.com>
2025-02-17 22:15:00 +0100

    Very minor auth changes

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
